67e31dce8a1a3c328c4711362612116f444b74fb
Furkan <ercelebifurkan@gmail.com>
2026-09-10 22:08:45 +0300

    .

--- ai-chat-bot/src/App.jsx
+++ ai-chat-bot/src/App.jsx
@@ -4,9 +4,18 @@ import { useState } from "react";
 
 const App = () => {
   const [isChatting, setIsChatting] = useState(false);
+  const [chats, setChats] = useState([]);
 
   const handleStartChat = () => {
     setIsChatting(true);
+
+    if(chats.length ===0) {
+      const newChat = {
+        id: `Chat ${new Date().toLocaleDateString("en-GB")} ${new Date().toLocaleTimeString()}`,
+        messages: []
+      }
+      setChats([newChat])
+    }
   };
 
   const handleGoBack = () => {
@@ -15,7 +24,7 @@ const App = () => {
 
   return (
     <div className="container">
-      {isChatting ? <ChatBotApp onGoBack={handleGoBack} />  : <ChatBotStart onStartChat={handleStartChat} /> }
+      {isChatting ? <ChatBotApp onGoBack={handleGoBack} chats={chats} setChats={setChats} />  : <ChatBotStart onStartChat={handleStartChat} /> }
     </div>
   );
 };
